a33816a09e0f1f83e52d03883734cc3734231695
Dhruba-Burlakoti <57421255+Dhruba-Burlakoti@users.noreply.github.com>
2024-02-01 13:14:20 -0700

    Update SimpLinR.R

--- SimpLin/R/SimpLinR.R
+++ SimpLin/R/SimpLinR.R
@@ -1,19 +1,3 @@
-#' Simple Linear Regression Using Rcpp
-#'
-#' This function performs a simple linear regression analysis on numeric vectors x and y using an RcppArmadillo backend.
-#' It validates the input, and then calls the C++ function `SimpLinCpp` to perform the computation.
-#' 
-#' @param x A numeric vector of predictor values.
-#' @param y A numeric vector of response values.
-#' 
-#' @return A list containing regression coefficients, standard errors, confidence intervals, residuals, and fitted values.
-#'
-#' @examples
-#' x <- 1:10
-#' y <- 2 + 3 * x + rnorm(10)
-#' result <- SimpLinR(x, y)
-#' print(result)
-#'
 #' @export
 #' @useDynLib SimpLin
 #' @importFrom Rcpp sourceCpp
